796b4f9d5f48ed428e3a9bd1098d87d12cf75321
Nathan Hallouin <wilhelm.rosental@ik.me>
2024-07-30 01:46:46 +0200

    some fixes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,16 @@
 import './index.scss'
 import React from 'react'
-// import { ReactComponent as Logo } from '../../assets/logo-secondary.svg'
+import { ReactComponent as Logo } from '../../assets/logo-secondary.svg'
 
 const Footer: React.FC = () => {
+  const currentYear = new Date().getFullYear()
+
   return (
     <footer className="footer">
-      {/* <Logo /> */}
-      <p className="fontSize-subtitle">&#169; 2020 Kasa. All rights Reserved</p>
+      <Logo />
+      <p className="fontSize-subtitle">
+        &#169; {currentYear} Kasa. All rights Reserved
+      </p>
     </footer>
   )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import './index.scss'
 import React from 'react'
-// import { ReactComponent as Logo } from '../../assets/logo-primary.svg'
+import { ReactComponent as Logo } from '../../assets/logo-primary.svg'
 import Navigation from '../Navigation'
 
 const Header: React.FC = () => {
@@ -8,7 +8,7 @@ const Header: React.FC = () => {
     <header className="header-main">
       <h1 className="sr-only">KASA</h1>
       <figure className="header-logo">
-        {/* <Logo /> */}
+        <Logo />
         <figcaption className="sr-only">Logo de Kasa</figcaption>
       </figure>
       <Navigation />
